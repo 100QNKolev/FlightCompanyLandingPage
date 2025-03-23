1cdec0f25d2b2064725a6fa047fc7002f4045e69
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-03-23 11:45:20 +0200

    Added Aurora lights

--- hero_mart/src/app/page.tsx
+++ hero_mart/src/app/page.tsx
@@ -1,5 +1,32 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import { motion } from "motion/react";
+import React from "react";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+
+export default function Page() {
+  return (
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          HeroMart
+        </div>
+        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+          Your one-stop shop for all your needs.
+        </div>
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+          Get Started
+        </button>
+      </motion.div>
+    </AuroraBackground>
+  );
 }
