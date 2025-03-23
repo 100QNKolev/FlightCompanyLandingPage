8c5ae991ee6112f21ba8b037f652cbdae865d016
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-03-23 12:06:04 +0200

    Added apple cards

--- hero_mart/src/app/page.tsx
+++ hero_mart/src/app/page.tsx
@@ -4,11 +4,13 @@ import { motion } from "motion/react";
 import React from "react";
 import { AuroraBackgroundDemo } from "@/components/-demo";
 import { AnimatedTestimonialsDemo } from "@/components/animated-testimonials-demo";
+import { AppleCardsCarouselDemo } from "@/components/apple-cards-carousel-demo";
 
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen">
       <AuroraBackgroundDemo />
+      <AppleCardsCarouselDemo />
       <AnimatedTestimonialsDemo />
     </main>
   );
