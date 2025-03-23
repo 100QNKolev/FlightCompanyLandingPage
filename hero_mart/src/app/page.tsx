319273e6da5beedd47c44ecf06d4c44ee660a10f
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-03-23 12:41:43 +0200

    Added globe

--- hero_mart/src/app/page.tsx
+++ hero_mart/src/app/page.tsx
@@ -2,16 +2,17 @@
 
 import { motion } from "motion/react";
 import React from "react";
-import { AuroraBackgroundDemo } from "@/components/-demo";
 import { AppleCardsCarouselDemo } from "@/components/apple-cards-carousel-demo";
 import { FloatingDockDemo } from "@/components/floating-dock-demo";
+import { CardHoverEffectDemo } from "@/components/card-hover-effect-demo";
+import { GlobeDemo } from "@/components/globe-demo";
 
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen">
+      <GlobeDemo />
       <FloatingDockDemo />
-
-      <AuroraBackgroundDemo />
+      <CardHoverEffectDemo />
       <AppleCardsCarouselDemo />
     </main>
   );
